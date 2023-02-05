0c7725719066b485d5073fd4fa1dc67b46caa71d
Kan <mreast1203@gmail.com>
2023-02-05 17:50:28 +0800

    add code for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,24 @@
+import path from "path";
+
 import express from "express";
 import cors from "cors";
 import db from "./db";
 import routes from "./routes";
 
 const app = express();
-app.use(cors());
+if (process.env.NODE_ENV === "development") {
+  app.use(cors());
+}
 app.use(express.json());
 routes(app);
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 4000;
 
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: "http://localhost:4000/api" });
-
+const API_ROOT =
+  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:4000/api";
+const instance = axios.create({
+  baseURL: API_ROOT,
+});
 export default instance;
